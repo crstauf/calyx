8afa6901bc03734a2fd332013d64314a4c113498
Caleb Stauffer <develop+github@calebstauffer.com>
2018-10-20 16:38:36 -0400

    add template part for default content

--- page.php
+++ page.php
@@ -4,32 +4,16 @@
  */
 
 get_header();
-?>
 
-<main class="container">
+echo '<main class="container">';
 
-	<?php
 	if ( have_posts() )
 		while ( have_posts() ) {
-			the_post()
-			?>
-
-			<article id="post-<?php the_ID() ?>" <?php post_class() ?>>
-
-				<h1><?php the_title() ?></h1>
-
-				<div class="entry">
-					<?php the_content() ?>
-				</div>
-
-			</article>
-
-			<?php
+			the_post();
+			Calyx()->get_template_part( 'parts/content.php' );
 		}
-	?>
 
-</main>
+echo '</main>';
 
-<?php
 get_footer()
 ?>
