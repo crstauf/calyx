7dc66090a6513c1e2fd23449005c907bd8cb96c1
Caleb Stauffer <develop+github@calebstauffer.com>
2018-11-08 00:21:32 -0500

    add throttle function

--- assets/js/calyx.js
+++ assets/js/calyx.js
@@ -8,6 +8,30 @@
 				Object.assign( this, _calyx_data );
 		},
 
+		/* https://remysharp.com/2010/07/21/throttling-function-calls */
+		throttle: function ( fn, threshhold, scope ) {
+			threshhold || ( threshhold = 250 );
+			var last,
+			deferTimer;
+			return function () {
+				var context = scope || this;
+				var now = +new Date,
+				args = arguments;
+
+				if ( last && ( now < ( last + threshhold ) ) ) {
+					// hold on to it
+					clearTimeout( deferTimer );
+					deferTimer = setTimeout( function () {
+						last = +new Date;
+						fn.apply( context, args );
+					}, threshhold + ( last - now ) );
+				} else {
+					last = now;
+					fn.apply( context, args );
+				}
+			};
+		},
+
 		/* https://davidwalsh.name/javascript-debounce-function */
 		debounce: function( func, wait, immediate ) {
 		 	var timeout;
